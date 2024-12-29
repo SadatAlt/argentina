61e2b8ac40c70ac7b063642b974d3df1c11db8b5
SadatAlt <arman142752@gmail.com>
2024-12-29 23:33:12 +0600

    changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -47,4 +47,52 @@ $(function(){
           }
           //seconds
         }, 0)
+        new VenoBox({
+          selector: '.my-image-links',
+          numeration: true,
+          infinigall: true,
+          share: true,
+          spinner: 'rotating-plane'
+      });
+        new VenoBox({
+          selector: '.my-video-links',
+      });
+      $(function(){
+        $(".typed").typed({
+          strings: ["1978.", "1986.", "2022."],
+          // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+          stringsElement: null,
+          // typing speed
+          typeSpeed: 50,
+          // time before typing starts
+          startDelay: 1500,
+          // backspacing speed
+          backSpeed: 30,
+          // time before backspacing
+          backDelay: 700,
+          // loop
+          loop: true,
+          // false = infinite
+          loopCount: 5,
+          // show cursor
+          showCursor: false,
+          // character for cursor
+          cursorChar: "|",
+          // attribute to type (null == text)
+          attr: null,
+          // either html or text
+          contentType: 'html',
+          // call when done callback function
+          callback: function() {},
+          // starting callback function before each string
+          preStringTyped: function() {},
+          //callback for every typed string
+          onStringTyped: function() {},
+          // callback for reset
+          resetCallback: function() {}
+        });
+      });
+      AOS.init({
+        duration:1000,
+      });
     }());
